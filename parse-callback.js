bca09765d84bf85818a154b890785bf6a7336122
gianfebrian <gianfebrian@gmail.com>
2016-02-22 22:47:48 +0700

    add line transformer solution

--- parse-callback.js
+++ parse-callback.js
@@ -9,45 +9,38 @@ const helper = require('./helper');
 // 0. Naïve
 
 function naive() {
-    fs.readFile(__dirname + '/sample.csv', function thenParse(err, loadedCsv) {
+  fs.readFile(__dirname + '/sample.csv', function thenParse(err, loadedCsv) {
+    parse(loadedCsv, function transformEachLine(err, parsed) {
+      for (let index in parsed) {
+        let line = parsed[index];
+        line = helper.lineTransformer(line);
 
-        parse(loadedCsv, function transformEachLine(err, parsed) {
+        if (index > 0) {
+          debug(`sending data index: ${index - 1}`);
+          helper.sendSms(line, function afterSending(err, sendingStatus) {
+            let lineToLog;
+            if (err) {
+              debug(err.message);
 
-            for (let index in parsed) {
-
-                let line = parsed[index];
-
-                // FIXME: Put your transformation here
-
-                if (index > 0) {
-                    debug(`sending data index: ${index - 1}`);
-
-                    helper.sendSms(line, function afterSending(err, sendingStatus) {
-                        let lineToLog;
-                        if (err) {
-                            debug(err.message);
-
-                            lineToLog = {
-                                sendingStatus,
-                                line,
-                            };
-                        }
-
-                        if (lineToLog) {
-                            helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
-                                if (err) {
-                                    debug(err.message);
-                                }
-                            });
-                        }
-                    });
-                }
-
-                index++;
+              lineToLog = {
+                sendingStatus,
+                line,
+              };
             }
-        });
+
+            if (lineToLog) {
+              helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
+                if (err) {
+                  debug(err.message);
+                }
+              });
+            }
+          });
+        }
+        index++;
+      }
     });
+  });
 }
 
 naive();
-
