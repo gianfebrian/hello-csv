31079d1e2ebdd0751c1732245f59ab1df6968f32
gianfebrian <gianfebrian@gmail.com>
2016-02-22 22:49:08 +0700

    add combination stream and asyncjs solution

--- parse-stream.js
+++ parse-stream.js
@@ -1,3 +1,49 @@
-// 0. Please use readline (https://nodejs.org/api/readline.html) to deal with per line file reading
-// 1. Then use the parse API of csv-parse (http://csv.adaltas.com/parse/ find the Node.js Stream API section)
+'use strict';
 
+const debug = require('debug')('hello');
+
+const async = require('async');
+
+const fs = require('fs');
+const readline = require('readline');
+
+const helper = require('./helper');
+const sendSms = helper.sendSms;
+const logToS3 = helper.logToS3;
+const lineTransformer = helper.lineTransformer;
+const lineParser = helper.lineParser;
+const sendSmsWithLogger = helper.sendSmsWithLogger(debug)(sendSms, logToS3);
+
+function parseWithStream() {
+  let fileStream = fs.createReadStream(__dirname + '/sample.csv');
+  let linesRead = readline.createInterface({
+    input: fileStream,
+  });
+
+  function parseStream(callback) {
+    let lines = [];
+    linesRead.on('line', (line) => {
+      lines.push(lineParser(line));
+    }).on('close', () => {
+      callback(null, lines);
+    });
+  }
+
+  function transformEachLine(parsedCsv, callback) {
+    async.forEachOf(parsedCsv, (line, index, eachCallback) => {
+      if (index > 0) {
+        line = lineTransformer(line);
+        sendSmsWithLogger(line, eachCallback);
+      }
+    }, callback);
+  }
+
+  async.waterfall([
+    parseStream,
+    transformEachLine,
+  ], (err, results) => {
+    debug('first log error callback (default behaviour of asyncjs)');
+  });
+}
+
+parseWithStream();
